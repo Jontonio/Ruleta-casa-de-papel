a77d993561b8da65d0c6e4915fbe9d077a444ca2
Jontonio <joseantoniorsystem@gmail.com>
2022-07-10 17:09:11 -0500

    add first version

--- js/index.js
+++ js/index.js
@@ -2,53 +2,31 @@
 window.onload = () =>{
     cargarRuleta();
 }
-
 // Variables globales
-var objRuleta;
-var winningSegment;
-var distnaciaX = 150;
-var distnaciaY = 50;
-var ctx;
+let objRuleta;
+let winningSegment;
+let disableButtom = false;
+let distnaciaX = 50;
+let distnaciaY = 50;
+let ctx;
+// instancia de audio
+let audioRuleta = new Audio('./assets/audio/ruleta.mp3');  // Create audio object and load desired file.
+let audioWin = new Audio('./assets/audio/win.mp3');  // Create audio object and load desired file.
 
 // Falta implementar el mensaje de ganador
 function Mensaje() {
     winningSegment = objRuleta.getIndicatedSegment();
     SonidoFinal();
-    swal({
-        title: " ¡ "+winningSegment.text+" !",
-    
-        imageUrl: "img/Muerte.png",
-        showCancelButton: true,
-        confirmButtonColor: "#e74c3c",
-        confirmButtonText: "Ok,Reiniciar",
-        cancelButtonText: "Quitar elemento",
-        closeOnConfirm: true,
-        closeOnCancel: true
-    },
-    function (isConfirm) {
-        if (isConfirm) {
-            
-        } else {
-
-            $('#ListaElementos').val($('#ListaElementos').val().replace(winningSegment.text,""));
-            leerElementos();
-            
-        }
-        objRuleta.stopAnimation(false);
-        objRuleta.rotationAngle = 0;
-        objRuleta.draw();
-        DibujarTriangulo();
-        bigButton.disabled = false;
-    });
+    winAnimation();
 }
 
-// Dibuja el triangulo que indica el numero que toco
+// Dibuja el triangulo que indica el número que toco
 function DibujarTriangulo() {
-    distnaciaX = 75;
-    distnaciaY = 5;
+    distnaciaX = 70;
+    distnaciaY = 30;
     ctx = objRuleta.ctx;
-    ctx.strokeStyle = 'navy';
-    ctx.fillStyle = '#000000';
+    ctx.strokeStyle = 'red';
+    ctx.fillStyle = '#FFFF';
     ctx.lineWidth = 2;
     ctx.beginPath();
     ctx.moveTo(distnaciaX + 170, distnaciaY + 5);
@@ -65,13 +43,15 @@ function DibujarRuleta(ArregloElementos) {
         objRuleta = new Winwheel({
             'canvasId': 'Ruleta',
             'numSegments': ArregloElementos.length,
-            'outerRadius': 250,
-            'innerRadius': 30,
+            'outerRadius': 220,
+            'innerRadius': 120,
+            'textFontFamily'  : 'Courier',
+            'textAligment' : 'inner',
             'segments':ArregloElementos,
             'animation':
             {
                 'type': 'spinToStop',
-                'duration':4,
+                'duration':7,
                 'spins': 15,
                 'callbackFinished': 'Mensaje()',
                 'callbackAfter': 'DibujarTriangulo()' 
@@ -85,23 +65,117 @@ function DibujarRuleta(ArregloElementos) {
 
 // funcion que carga los elementos de la ruleta
 function cargarRuleta() {
-        var ElementosRuleta= [];
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "10" });
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "20" });
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "50" });
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "100" });
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "110" });
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "70" });
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "40" });
-        ElementosRuleta.push({ 'fillStyle': "#" + ((1 << 24) * Math.random() | 0).toString(16), 'text': "80" });
+
+        var ElementosRuleta = [];
+
+        ElementosRuleta.push({ 
+                                'fillStyle': "#C91F37", 
+                                'text': "S/100",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
+        ElementosRuleta.push({ 
+                                'fillStyle': "#DC3023", 
+                                'text': "S/20",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
+        ElementosRuleta.push({ 
+                                'fillStyle': "#9D2933", 
+                                'text': "S/50",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
+        ElementosRuleta.push({ 
+                                'fillStyle': "#CF000F", 
+                                'text': "S/100",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
+        ElementosRuleta.push({ 
+                                'fillStyle': "#F22613", 
+                                'text': "S/110",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
+        ElementosRuleta.push({ 
+                                'fillStyle': "#8F1D21",
+                                'text': "S/70",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
+        ElementosRuleta.push({ 
+                                'fillStyle': "#CF3A24", 
+                                'text': "S/40",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
+        ElementosRuleta.push({ 
+                                'fillStyle': "#9D2933", 
+                                'text': "S/80",
+                                'textOrientation' : 'curved',
+                                'textAligment' : 'center',
+                                'textFontSize' : 25,
+                                'textFillStyle':'#FFFFFF'
+                            });
         DibujarRuleta(ElementosRuleta);
 } 
 
-// Falta implementar
-var audio = new Audio('alarma.mp3');  // Create audio object and load desired file.
+// funcion win animation
+function winAnimation(){
+
+    // Get the number of the winning segment.
+    let winningSegmentNumber = objRuleta.getIndicatedSegmentNumber();
+
+    // Loop and set fillStyle of all segments to gray.
+    for (let x = 1; x < objRuleta.segments.length; x ++) {
+        objRuleta.segments[x].fillStyle = 'gray';
+    }
+
+    // Make the winning one yellow.
+    objRuleta.segments[winningSegmentNumber].fillStyle = '#FF0000';
+
+    // Call draw function to render changes.
+    objRuleta.draw();
+
+    // Also re-draw the pointer, otherwise it disappears.
+    DibujarTriangulo();
+}
+
+function resetRuleta(){
+
+    cargarRuleta();
+    disableButtom = false;
+}
+
+// start game 
+function start(){
+
+    if(!disableButtom) {
+        objRuleta.startAnimation();
+        audioRuleta.pause();
+        audioRuleta.currentTime = 0;
+        audioRuleta.play();
+    }
+    disableButtom = true;
+}
 
 function SonidoFinal(){
-    audio.pause();
-    audio.currentTime = 0;
-    audio.play();
+    audioWin.pause();
+    audioWin.currentTime = 0;
+    audioWin.play();
 }
